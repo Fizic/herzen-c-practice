8568ed7545dd14b5ba4f2c960411b0227cccc173
Firsov Kirill <kirya.firsov.04@gmail.com>
2023-04-02 15:46:57 +0300

    fix: 1.7 in lab 2

--- lab_2/5.cpp
+++ lab_2/5.cpp
@@ -7,4 +7,6 @@ int main() {
     long long *arr = (long long *) malloc(sizeof(long long*) * n);
     for(int i = 0; i < n; i++) scanf("%lld", &arr[i]);
     for(int i = n - 1; i > -1; --i) printf("%lld ", *(arr + i));
+    free(arr);
+    return EXIT_SUCCESS;
 }
--- lab_2/7.cpp
+++ lab_2/7.cpp
@@ -1,17 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define idx(i, j) (i*m + j)
+int **allocate_matrix(long long n, long long m) {
+    void *ptr = malloc(n * sizeof(int *) + n * m * sizeof(int));
+    int **a = (int **) ptr;
+    int *aData = (int *) (a + n);
+
+    for(int i = 0; i < n; i++){
+        a[i] = aData + i * m;
+    }
+
+    return a;
+}
 
 int main() {
     long long n, m;
-    scanf("%lld %lld", n, m);
-    int *arr = malloc(n * m * sizeof *arr);
+    scanf("%lld %lld", &n, &m);
+    int **arr = allocate_matrix(n, m);
 
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            arr[idx(i, j)] = i + j;
+            arr[i][j] = i + j;
+            printf("%d ", arr[i][j]);
         }
+        printf("\n");
     }
 
     free(arr);
